efdefeff6a90d0b0ba295444f4690b4d4f946825
ashin paul <ashinpaul46@gmail.com>
2023-07-14 09:56:38 +0530

    ok

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connstr = "server=localhost;user=root;database=sys;port=3306;password=rajagiri";
+            string sql = "select *from mytable";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connectiong to mysql");
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+            }
+        }
+    }
+}
